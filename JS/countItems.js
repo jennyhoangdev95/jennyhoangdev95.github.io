baf0d7c313a00b5ce500c7d11fa52e30f381d713
Duykhuong97 <duykhuong9a@gmail.com>
2023-05-14 17:48:37 +0700

    update checkout form

---
+++ JS/countItems.js
@@ -0,0 +1,7 @@
+var countItem = 0
+document.getElementById("countItem").value = 0
+
+function addProduct(){
+    countItem ++
+    document.getElementById("countItem").value = countItem
+}
--- JS/product.js
+++ JS/product.js
@@ -105,12 +105,3 @@ for (let index = 0; index < mockData.length; index++) {
 getCardEle.innerHTML = html
 
 
-
-var countItem = 0
-document.getElementById("countItem").value = 0
-
-function addProduct(){
-    countItem ++
-    document.getElementById("countItem").value = countItem
-}
-
