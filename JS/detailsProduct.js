6aa40e442e3c245db9699f8c6fbcaf860ae0d80d
jennyhoangdev95 <131568984+jennyhoangdev95@users.noreply.github.com>
2023-05-14 16:29:24 +0700

    update slider product of details product page

--- JS/detailsProduct.js
+++ JS/detailsProduct.js
@@ -2,19 +2,21 @@
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
     fade: false,
+    arrows: false,
     asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
+});
+$('.slider-nav').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     centerMode: true,
     focusOnSelect: true,
+    arrows: true,
     infinite: false,
     prevArrow:
         "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:
+    nextArrow:
         "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
-  });
+});
+
